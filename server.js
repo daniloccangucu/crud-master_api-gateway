338be8bcb2abf184351ec3479a659222a0a7406b
Danilo Canguçu <daniloccangucu@gmail.com>
2024-05-21 19:59:55 +0300

    server using .env PORT var

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 import express from "express";
 import pkg from "body-parser";
 import routes from "./routes.js";
+import dotenv from "dotenv";
+import proxy from "./proxy.js";
+
+dotenv.config();
 
 const { json } = pkg;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.GATEWAY_PORT;
 
 app.use(json());
+
+app.use("/api/movies", proxy);
 app.use("/api", routes);
 
 app.listen(PORT, () => {
