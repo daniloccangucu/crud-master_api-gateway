9239c87c33bf6eca9e41fab65851feb22288900a
Danilo Canguçu <daniloccangucu@gmail.com>
2024-05-21 19:59:23 +0300

    proxy with custom proxy

--- proxy.js
+++ proxy.js
@@ -1,19 +1,53 @@
-import { connect } from "amqplib";
+import http from "http";
+import https from "https";
+import url from "url";
 
-const QUEUE_NAME = process.env.RABBITMQ_QUEUE;
-const RABBITMQ_URL = process.env.RABBITMQ_URL;
+function customProxy(req, res) {
+  const targetUrl = process.env.INVENTORY_API_URL;
+  const parsedUrl = url.parse(targetUrl);
 
-export async function publishToRabbitMQ(message) {
-  try {
-    const connection = await connect(RABBITMQ_URL);
-    const channel = await connection.createChannel();
-    await channel.assertQueue(QUEUE_NAME);
-    channel.sendToQueue(QUEUE_NAME, Buffer.from(JSON.stringify(message)));
-    console.log("Message published to RabbitMQ:", message);
-    await channel.close();
-    await connection.close();
-  } catch (error) {
-    console.error("Error publishing message to RabbitMQ:", error);
-    throw error;
+  const options = {
+    hostname: parsedUrl.hostname,
+    port: parsedUrl.port,
+    path: parsedUrl.pathname + req.url.replace("/api/movies", ""),
+    method: req.method,
+    headers: {
+      ...req.headers,
+      host: parsedUrl.hostname,
+    },
+  };
+
+  console.log(
+    `Proxying ${req.method} request to: ${options.hostname}:${options.port}${options.path}`
+  );
+
+  const proxy = (parsedUrl.protocol === "https:" ? https : http).request(
+    options,
+    function (proxyRes) {
+      console.log(`Response from target: ${proxyRes.statusCode}`);
+      proxyRes.pipe(res, { end: true });
+      res.writeHead(proxyRes.statusCode, proxyRes.headers);
+    }
+  );
+
+  proxy.on("error", function (err) {
+    console.error("Proxy error:", err);
+    res.status(500).send("Proxy error");
+  });
+
+  if (req.method === "POST" || req.method === "PUT") {
+    const bodyData = JSON.stringify(req.body);
+    console.log("Forwarding body:", bodyData);
+    proxy.setHeader(
+      "Content-Type",
+      req.headers["content-type"] || "application/json"
+    );
+    proxy.setHeader("Content-Length", Buffer.byteLength(bodyData));
+    proxy.write(bodyData);
+    proxy.end();
+  } else {
+    req.pipe(proxy, { end: true });
   }
 }
+
+export default customProxy;
